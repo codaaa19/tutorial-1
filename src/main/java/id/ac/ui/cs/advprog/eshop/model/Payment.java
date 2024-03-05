7e45a963caa774971483fea56d10f9c340706dc5
codaaa19 <tsabitiphone@gmail.com>
2024-03-05 16:19:32 +0700

    [RED] Adding Payment Model Skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,47 +1,24 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import java.util.Map;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.Arrays;
-
-import java.util.List;
-import java.util.Map;
 
 @Builder
 @Getter
 public class Payment {
     String id;
-    Map<String,String> paymentData;
     String method;
+    Order order;
+    Map<String, String> paymentData;
+    @Setter
     String status;
 
-    public Payment (String id, Map<String,String> paymentData, String method){
-        this.id = id;
-        this.method = method;
-        this.status = OrderStatus.WAITING_PAYMENT.getValue();
-
-        if(paymentData.isEmpty()){
-            throw new IllegalArgumentException();
-        }
-        else{
-            this.paymentData = paymentData;
-        }
+    public Payment(String id, String method, Order order, Map<String, String> paymentData) {
     }
 
-    public Payment(String id, Map<String,String> paymentData, String method, String status){
-        this(id,paymentData,method);
-        this.setStatus(method, status);
-    }
-
-    public void setStatus(String payment,String status) {
-        if (OrderStatus.contains(status)){
-            this.status = status;
-        }
-        else {
-            throw new IllegalArgumentException();
-        }
+    public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {
     }
 }
-
