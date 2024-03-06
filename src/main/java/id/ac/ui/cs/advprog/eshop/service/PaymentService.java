22e8c6c45950e9894a58c0ed2ab9e789eddc5165
codaaa19 <tsabitiphone@gmail.com>
2024-03-06 19:25:06 +0700

    [REFACTORING] Adding function

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -9,5 +9,6 @@ public interface PaymentService {
     public Payment addPayment (Order order, String method, Map<String,String> paymentData);
     public Payment getPayment (String paymentId);
     public Payment getAllPayments();
-    Payment setStatus(String id, String status);
+    public Payment getPaymentById(String id);
+    public Payment setStatus(String id, String status);
 }
