86f0e98e04de7097b59bb92037780697ce7efce0
codaaa19 <tsabitiphone@gmail.com>
2024-03-06 16:38:50 +0700

    [RED] Adding BankPayment skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/BankPayment.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public class BankPayment extends Payment {
+    public BankPayment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+        super(id, method, order, paymentData, status);
+    }
+
+    public BankPayment(String id, String method, Order order, Map<String, String> paymentData) {
+        super(id, method, order, paymentData);
+    }
+}
