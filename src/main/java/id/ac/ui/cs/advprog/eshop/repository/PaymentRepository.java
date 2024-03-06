113ce90aea0ed76974bc612f6a5c7fa122cba8eb
codaaa19 <tsabitiphone@gmail.com>
2024-03-06 18:33:59 +0700

    [GREEN] Implement PaymentRepo

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,42 +1,38 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.ui.cs.advprog.eshop.model.Payment;
-import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+@Repository
 public class PaymentRepository {
-    private List<Payment> paymentData = new ArrayList<>();
-
-    public Payment save(Payment payment){
+    private List<Payment> listPayments = new ArrayList<>();
+    public Payment save(Payment payment) {
         int i = 0;
-        for (Payment savedPayment : paymentData){
-            if (savedPayment.getId().equals(payment.getId())){
-                paymentData.remove(i);
-                paymentData.add(i,payment);
-                return  payment;
+        for (Payment p : listPayments) {
+            if (p.getId().equals(payment.getId())) {
+                listPayments.set(i, payment);
+                return payment;
             }
-            i += 1;
+            i++;
         }
-        paymentData.add(payment);
+
+        listPayments.add(payment);
         return payment;
     }
-
-    public Payment findById(String id){
-        for (Payment curPayment : paymentData){
-            if(curPayment.getId().equals(id)){
-                return curPayment;
+    public Payment findById(String id) {
+        for (Payment p : listPayments) {
+            if (p.getId().equals(id)) {
+                return p;
             }
         }
-        return  null;
-    }
 
-//    public List<Payment> findAllPaymentByAuthor (String author){
-//        List<Payment> result = new ArrayList<>();
-//        for (Payment savedPayment : paymentData){
-//            if (savedPayment.get)
-//        }
-//    }
+        return null;
+    }
+    public List<Payment> getAllPayments() {
+        return listPayments;
+    }
 }
